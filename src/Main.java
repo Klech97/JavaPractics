b5f5106b1360f2fdb39b57dc8d73bda8d3a2f137
VadimT <dflbv.tor@gmail.com>
2025-01-11 12:46:18 +0900

    create class Transport

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Transport car = new Transport(300);
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
---
+++ src/Transport.java
@@ -0,0 +1,3 @@
+public class Transport {
+    public Transport(int speed);
+}
